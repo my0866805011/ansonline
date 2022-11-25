bca1e1b98f9e06692e08f1c66cee9b59c1370a02
my0866805011 <my0866805011@gmail.com>
2022-11-26 00:34:36 +0700

    12

--- lib/states/create_acc.dart
+++ lib/states/create_acc.dart
@@ -10,17 +10,57 @@ class CreateAcc extends StatefulWidget {
 }
 
 class _CreateAccState extends State<CreateAcc> {
+  Row nameMethod(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: EdgeInsets.only(top: 16),
+          width: size * 0.6,
+          child: TextFormField(
+            decoration: InputDecoration(
+              labelStyle: Static_val().h3Style(),
+              labelText: 'User :',
+              prefixIcon: Icon(Icons.account_circle, color: Static_val.light),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Static_val.dart),
+                borderRadius: BorderRadius.circular(30),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Static_val.light),
+                borderRadius: BorderRadius.circular(30),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    double size = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         title: Text('Create New Account'),
         backgroundColor: Static_val.primary,
       ),
-      body: ShowTitle(title: 'ข้อมูลทั่วไป',
-      textStyle: Static_val().h2Style(),
+      body: ListView(
+        children: [
+         showTitle('ข้อมูลทั่วไป'),
+         nameMethod(size),
+         showTitle('เลือก ประเภท'),
+        ],
       ),
+    );
+  }
 
+  Container showTitle(String title) {
+    return Container(margin: EdgeInsets.symmetric(vertical: 16),
+      child: ShowTitle(
+        title: title,
+        textStyle: Static_val().h2Style(),
+      ),
     );
   }
 }
